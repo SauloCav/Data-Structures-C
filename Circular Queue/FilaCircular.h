6db349f30adb9249be7d6f5cb42b4db3127d38a5
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-07 10:14:37 -0300

    adds circular queue .h file

---
+++ Circular Queue/FilaCircular.h
@@ -0,0 +1,14 @@
+typedef struct filaCircular Fila;
+
+Fila* create(int n); //
+
+int add(Fila* fi, int e); //
+
+int remove(Fila* fi); //
+
+int isFull(Fila* fi); //
+
+int isEmpty(Fila* fi); //
+
+void printFila(Fila* fi); //
+
