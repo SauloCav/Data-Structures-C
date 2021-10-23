8dec1a5d3ec29e101f5b24dd67e5b3cf296ed0b3
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-23 11:17:27 -0300

    adds corrections in .h file in queue

--- Queue/fila.h
+++ Queue/fila.h
@@ -1,24 +1,23 @@
 typedef struct fila Fila;
 
-Fila* create(int n); //
+Fila* create(int n);
 
-int add(Fila* fi, int e); //
+int add(Fila* fi, int e);
 
-int pop(Fila* fi); //
+int pop(Fila* fi);
 
-int size(Fila* fi); //
+int size(Fila* fi);
 
-int first(Fila* fi); //
+int first(Fila* fi);
 
-int last(Fila* fi); //
+int last(Fila* fi);
 
-int find(Fila* fi, int y, int *vetor, int j); //
+int find(Fila* fi, int y, int *vetor, int j);
 
-int isFull(Fila* fi); //
+int isFull(Fila* fi);
 
-int isEmpty(Fila* fi); //
+int isEmpty(Fila* fi);
 
-void clear(Fila* fi); //
+void clear(Fila* fi);
 
-void printFila(Fila* fi); //
-
+void printFila(Fila* fi);
