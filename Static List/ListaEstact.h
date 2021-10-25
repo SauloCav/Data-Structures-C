88f8ddbf73d88c6cb1a0e627d09c216855741159
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-25 10:29:48 -0300

    adds corrections in .h file in static list

--- Static List/ListaEstact.h
+++ Static List/ListaEstact.h
@@ -1,24 +1,23 @@
 typedef struct lista Lista;
 
-Lista* create(int n); //
+Lista* create(int n);
 
-int add(Lista* li, int e); //
+int add(Lista* li, int e);
 
-int addAtPosition(Lista* li, int i, int x); //
+int addAtPosition(Lista* li, int i, int x);
 
-int remove(Lista* li, int x);  //
+int remove(Lista* li, int x); 
 
-int removeAtPosition(Lista* li, int i, int x); //
+int removeAtPosition(Lista* li, int i, int x);
 
-int size(Lista* li); //
+int size(Lista* li);
 
-int find(Lista* li, int y, int *vetor, int j); //
+int find(Lista* li, int y, int *vetor, int j);
 
-int isFull(Lista* li); //
+int isFull(Lista* li);
 
-int isEmpty(Lista* li); //
+int isEmpty(Lista* li);
 
-int clear(Lista* li); //
+int clear(Lista* li);
 
-void printLista(Lista* li); //
-
+void printLista(Lista* li);
