6cabc83e7f85432c15ea7b30d55279d8248fcbf3
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-27 09:08:39 -0300

    adds .h file in linked list structure

---
+++ Linked List/ListaEncadeada.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "No.h"
+ 
+typedef struct {
+	 No *inicio;
+	 No *fim;
+	 int tamanho;
+} ListaEncadeada;
+
+ListaEncadeada* create(); //
+
+int add(ListaEncadeada* lista, int valor); //
+
+int remove(ListaEncadeada *lista, int chave); //
+
+int find(ListaEncadeada* lista, int c); //
+
+int isEmpty(ListaEncadeada* lista); //
+
+int size(ListaEncadeada* lista); //
+
+void clear(ListaEncadeada* lista); //
+
+void printListaEncadeada(ListaEncadeada* listaEncadeada); //
