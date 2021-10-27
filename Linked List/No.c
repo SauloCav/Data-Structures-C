4b588a27ac22db3c029a4cffc609a81d77aced8b
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-27 09:23:26 -0300

    make corrections in no.c file in linked list structure

--- Linked List/No.c
+++ Linked List/No.c
@@ -2,62 +2,39 @@
 #include <stdlib.h>
 #include "No.h"
 
-//////////////////////// CREATENO /////////////////////////////////////////////////////////////////////////////////////////////////
-
 No* createNo(int v, No* proximo){
  	
- 	No * n = (No*) malloc(sizeof(No)); // 1 | 1
+ 	No * n = (No*) malloc(sizeof(No));
  	
- 	n->valor = v; // 1 
- 	n->proximo = proximo; // 1
+ 	n->valor = v; 
+ 	n->proximo = proximo;
  	
- 	return n; // 1
+ 	return n; 
  	
 }
-
-// Complexidade temporal: O(4) = O(c) (Constante)
-// Complexidade espacial: O(1) = O(c) (Constante)
-
-//////////////////////// PRINTNO //////////////////////////////////////////////////////////////////////////////////////////////////
  
 void printNo(No* n){
  	
- 	if(n != NULL && n->proximo != NULL) // 2
+ 	if(n != NULL && n->proximo != NULL)
  		printf("No [valor: %i, proximo: %i]\n", n->valor, n->proximo->valor);
- 	else if(n != NULL) // 1
+ 	else if(n != NULL)
  		printf("No [valor: %i, proximo: NULL]\n", n->valor);
  	else
  		printf("NULL\n");
  	
 }
 
-// Complexidade temporal: O(3) = O(c) (Constante)
-// Complexidade espacial: O(0) = O(c) (Constante)
-
-//////////////////////// IGUALDADE ////////////////////////////////////////////////////////////////////////////////////////////////
-
 int igualdade(No* n, int c){
 	
-	if(c==n->valor){ // 1
-		return 1; // 1
+	if(c==n->valor){
+		return 1;
 	}
 	else{
-		return 0; // 1
+		return 0;
 	} 
 	
 }
-
-// Complexidade temporal: O(3) = O(c) (Constante)
-// Complexidade espacial: O(0) = O(c) (Constante)
-
-//////////////////////// FREENO ///////////////////////////////////////////////////////////////////////////////////////////////////
  
 void freeNo(No* n){
- 	free(n); // 1
+ 	free(n);
 }
-
-// Complexidade temporal: O(1) = O(c) (Constante)
-// Complexidade espacial: O(0) = O(c) (Constante)
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
