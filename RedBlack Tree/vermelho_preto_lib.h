63415ed89caacc0996c2d9643ba401bb0323b332
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-09-11 11:55:45 -0300

    Update vermelho_preto_lib.h

--- RedBlack Tree/vermelho_preto_lib.h
+++ RedBlack Tree/vermelho_preto_lib.h
@@ -10,19 +10,14 @@ typedef struct no{
 	
 	struct no* esq;
 	struct no* dir;
-
 }Tno;
 
 void menu();
 Tno* rotacao_simples_esquerda(Tno *a);
 Tno* rotacao_simples_direita(Tno *a);
-
 Tno* criar(int elemento);
 Tno* incluir_no(Tno* a, Tno* dado);
 Tno* inserir(Tno* a, int valor);
-
 int cor(Tno* a);
 void trocaCor(Tno* a);
-
 void imprime(Tno* a);
-
