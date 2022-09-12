756e5d5008c6dc79443fce5e34f26ffd9ae1e156
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-09-12 09:17:43 -0300

    Update main.cpp

--- Simple Heap/main.cpp
+++ Simple Heap/main.cpp
@@ -2,23 +2,20 @@
 
 int main(void) {
 	int op, valor, maximo_elementos;
-	
-	//--------- iniciando a estrutura --------------------//
+
     puts("Informe o numero maximo de elementos do heap: ");
     scanf("%d", &maximo_elementos);
     heap *h = criar(maximo_elementos);
     
     do{    
-    //--------------- Exibir menu ---------------------//
         system("cls");
         puts("\n\t\t\t\tHEAP\n");
         puts("\t1  - INSERIR ELEMENTO\n \t2  - RETIRAR ELEMENTO\n \t3  - MOSTRAR TODA ESTRUTURA\n\t4 - LIBERAR TODA ESTRUTURA\n\t0  - SAIR");
         printf("\nINFORME SUA OPCAO:\n");
-        scanf("%d", &op);//Escolha da Opção
+        scanf("%d", &op);
         
         switch(op)
         {
-        //Opção de exibir lista
     		case 1:{
     			printf("Informe um elemento: ");
     			scanf("%d", &valor);
@@ -26,7 +23,6 @@ int main(void) {
     			mostrar(h);
 				break;
 			}
-    		
     		case 2:{
     			puts("Voce escolheu tirar o topo \n");
     			valor = heap_retira(h);
@@ -34,13 +30,10 @@ int main(void) {
     			mostrar(h);
 				break;
 			}
-    			
     		case 3:mostrar(h); break;
-    			
     		case 4: heap_libera(h); break;
 	    	default:
 	    			puts("OPCAO INCORRETA,TENTAR NOVAMENTE.");break;
-	      
 	        }
 		getch();
 	    }while(op!=0);
